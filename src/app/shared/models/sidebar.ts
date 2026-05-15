c35de6e41a65542b66e4241de7aed79a00e93eff
owono eloundou <owonohermann54@gmail.com>
2026-05-15 16:40:39 +0200

    Fix: retrait de la fonctionnalité de tri des conducteurs

---
+++ src/app/shared/models/sidebar.ts
@@ -0,0 +1,30 @@
+export const SidebarItems = [
+    {
+        'label': 'Dashboard',
+        'path': 'dashboard'
+    },
+    {
+        'label': 'Utilisateurs',
+        'path': 'users'
+    },
+    {
+        'label': 'Conducteurs',
+        'path': 'drivers'
+    },
+    {
+        'label': 'Garants',
+        'path': 'guarantors'
+    },
+    {
+        'label': 'Quiz',
+        'path': 'quiz'
+    },
+    {
+        'label': 'Type de fichiers',
+        'path': 'types/file'
+    },
+    {
+        'label': 'Type de véhicules',
+        'path': 'car/types'
+    }
+]
